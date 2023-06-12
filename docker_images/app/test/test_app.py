acadf97c4701aecac835de23610499d4c6d4a151
kaito-h <kaito.hayafune@pasonadigisol.jp>
2023-06-12 10:02:01 +0900

    テスト仮作成

---
+++ docker_images/app/test/test_app.py
@@ -0,0 +1,10 @@
+import pytest
+from app import app
+
+
+# test_page_post_jsonのテストコード
+def test_test_page_post_json():
+    with app.test_client() as c:
+        response = c.post("/test_post_json", json={"password": "test_password"})
+        assert response.status_code == 200
+        assert response.json == {"password": "test_password"}
